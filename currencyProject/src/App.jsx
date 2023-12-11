d35984777fd4450e25a9d98e0b8855988a46e601
rakshith_s <rakshithsantosh@gmail.com>
2023-12-11 08:17:04 +0530

    added functionality

--- currencyProject/src/App.jsx
+++ currencyProject/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import {InputBox} from './components'
-import useCurrencyInfo, {useCurrencyInfo} from './hooks/useCurrencyInfo'
+import {InputBox} from './components/InputBox'
+import {useCurrencyInfo} from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
